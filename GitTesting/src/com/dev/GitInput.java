0e286b3e0072efea59af35eaf2160b086900c298
rahulwalia09 <rahulwalia1995@gmail.com>
2019-08-05 17:20:54 +0530

    New Rule

---
+++ GitTesting/src/com/dev/GitInput.java
@@ -0,0 +1,20 @@
+package com.dev;
+
+public class GitInput {
+private int num=0;
+
+/**
+ * @return the num
+ */
+public int getNum() {
+	return num;
+}
+
+/**
+ * @param num the num to set
+ */
+public void setNum(int num) {
+	this.num = num;
+}
+
+}
---
+++ GitTesting/src/com/dev/GitOutput.java
@@ -0,0 +1,20 @@
+package com.dev;
+
+public class GitOutput {
+private int answer=0;
+
+/**
+ * @return the answer
+ */
+public int getAnswer() {
+	return answer;
+}
+
+/**
+ * @param answer the answer to set
+ */
+public void setAnswer(int answer) {
+	this.answer = answer;
+}
+
+}
